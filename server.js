15ffe98c06a0bd1cdfdf602b318a2e3aaef249f2
Carlos Timaury <carlos.timaury@oowlish.com>
2021-08-13 15:53:40 -0300

    added game constructor

--- server.js
+++ server.js
@@ -10,18 +10,12 @@ const io = new Server(server);
 
 app.use('/games/:player', express.static(path.join(__dirname, "public")))
 
-app.get('/games/:player', (req, res) => {
-    res.send("<b>Just 2 players allowed, please go to /games/1 or games/2<b>")
-    // if (req.params.player == '1' || req.params.player == '2')
-    //     res.sendFile(__dirname + '/public/index.html');
-    // else
-});
 
 io.on('connection', (socket) => {
     console.info("A connection was established")
-    socket.on('play', (path) => {
-        console.log(`A play was made from ${path}`)
-        io.emit('play', `${path}`);
+    socket.on('play', (player, index) => {
+        console.log(`A play was made from ${player} on square ${index}`)
+        io.emit('play', player, index);
     });
 });
 
